df7dea4c1913fd8f4b3fb0b736f65e9f8d0ef618
Matthew <mvd7793@gmail.com>
2012-12-02 19:03:57 -0600

    Prettier injection with an icon and such. Next step is to make it work and stuff.

--- gmail.js
+++ gmail.js
@@ -24,7 +24,16 @@ GmailScheduler.prototype.setup = function() {
     var sendTable = null;
     try {
         // This is approximately the worst thing ever, but it works. If there's a better way (ideally without having to use jQuery - it's a bunch of extra JS to load when I don't actually need it), someone please let me know before I devolve into a weeping pile of tears.
-        sendTableContainer = document.querySelector('div.nH.nn[style="width: 455px; height: 712px;"]').querySelector('div.nH.Hd[role="dialog"]').querySelector("form").parentNode.parentNode.parentNode.parentNode.querySelector('colgroup').parentNode.parentNode;
+
+        // Step 1 is to find the top container
+        var topContainer = document.querySelector('div.nH.nn[style="width: 455px; height: 712px;"]');
+
+        // And the first one might not have worked, so let's try another if it didn't
+        if (topContainer == null) {
+            topContainer = document.querySelector('div.nH.nn[style="width: 455px; height: 668px;"]');
+        }
+
+        sendTableContainer = topContainer.querySelector('div.nH.Hd[role="dialog"]').querySelector("form").parentNode.parentNode.parentNode.parentNode.querySelector('colgroup').parentNode.parentNode;
     } catch(err) {
         // It's possible the compose window is not open, so we shouldn't execute the setup, and we'll keep checking for when it might open up
         return;
@@ -41,11 +50,33 @@ GmailScheduler.prototype.setup = function() {
     row.appendChild(col);
 
     row.setAttribute('class', 'gU');
-    col.setAttribute('style', 'min-width: 448px; font-size: 80%;');
+    col.setAttribute('style', 'min-width: 438px; font-size: 80%;');
 
-    col.innerHTML = 'or delay sending: <input type="text"></input>';
+    col.innerHTML = '<span style="display: inline"> \
+    <img style="width: 18px; vertical-align: middle; margin-left: 3px;" src="'
+    + chrome.extension.getURL('icon.png') +  '"> \
+    </span> \
+    or send later: \
+    <form style="display: inline-block"> \
+        <select> \
+            <option>in an hour</option> \
+            <option>in 6 hours</option> \
+            <option>tomorrow, 11AM</option> \
+            <option>in a day</option> \
+            <option>in a week</option> \
+            <option>Custom</option> \
+        </select> \
+        <button style="width: 80px;"> \
+            Send Later \
+        </button> \
+    </form>';
+
+
+    var hr = document.createElement('hr');
+    hr.setAttribute('style', 'margin: 2px; border-color: #cfcfcf; border-top-width: 1px; border-style: solid; border-bottom-width: 0px;');
 
     // add row
+    sendTableContainer.appendChild(hr);
     sendTableContainer.appendChild(table);
 
     window.clearInterval(this.setupTimer);
